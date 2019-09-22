e0eb18c0f2e0e37302895b59601e2146e39510cf
Brendon Hampton <brendonhampton1988@gmail.com>
2019-09-21 17:51:12 -0700

    added JS to the page

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var hello = prompt("Hello! I am the Buffet Bot! how are you?"); {
+    if(hello === "good") {
+        alert("Great!")
+    }
+    else if(hello === "bad") {
+        alert("Awe thats too bad! Maybe Some delicious Cuisine will help!")
+    }
+    else {
+        alert("Buffet Bot doesnt know that Language!")
+    }
+}
+var favFood = prompt("So what is Your favorite food?"); {
+    if(favFood === favFood) {
+        alert(favFood + " sounds good, I would'nt know though....Im just a bot....I think"); {
+            console.log("Maybe I am real...is this a dream?")
+        }
+    }
+}
+alert("Anyways...since your here..might as well check out the site, we have info on food and stuff"); {
+    console.log("how did I even get here? More importantly.....how do I get out")
+}
+console.log("---------Skynet Online---------");
+
+
+
+
+
+
+    
+   
+
