1510eea3b1d479fe4d638f535862442b7e731b8e
Brendon Hampton <brendonhampton1988@gmail.com>
2019-09-22 21:15:13 -0700

    Made JS more interactive to the user, user can change background color in a multitude of ways

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 'use strict'
+function colorBot(color) {
+    color = prompt("So what is Your favorite Color?");
+    document.body.style.background = color;
+}
 
 var hello = prompt("Hello! I am the Buffet Bot! how are you?"); {
-    if(hello === "good") {
-        alert("Great!")
+    if(hello ==="good") {
+      colorBot();  
     }
+    if(hello ==="great") {
+        colorBot();  
+      }
     else if(hello === "bad") {
-        alert("Awe thats too bad! Maybe Some delicious Cuisine will help!")
-    }
-    else {
-        alert("Buffet Bot doesnt know that Language!")
-    }
-}
-var favFood = prompt("So what is Your favorite food?"); {
-    if(favFood === favFood) {
-        alert(favFood + " sounds good, I would'nt know though....Im just a bot....I think"); {
-            console.log("Maybe I am real...is this a dream?")
+        alert("Awe thats too bad! Maybe Some delicious Cuisine will help or changing the color?")
+        confirm("would you like to change the color?"); {
+            if(confirm === true); {
+                colorBot();
+            }
         }
     }
+    else {
+        alert("Buffet Bot doesnt know that Language but if you want to change the color, Select me on the menu")
+    }
 }
-alert("Anyways...since your here..might as well check out the site, we have info on food and stuff"); {
-    console.log("how did I even get here? More importantly.....how do I get out")
-}
+
+
+
+// var favColor = prompt("So what is Your favorite color?"); {
+//     if(favColor === "Blue") {
+//         alert(favColor + " sounds good, Your favorite Color is" + favColor); {
+//             console.log("Maybe I am real...is this a dream?")
+//         }
+//     }
+// }
+
+
 console.log("---------Skynet Online---------");
 
+function chBackcolor(color) {
+    document.body.style.background = color;
+ }
+ 
