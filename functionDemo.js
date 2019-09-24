8997b044cea9a9f9815a961d6bef042c26f52467
Brendon Hampton <brendonhampton1988@gmail.com>
2019-09-23 20:56:37 -0700

    Removed images and changed sloppy code

---
+++ functionDemo.js
@@ -0,0 +1,12 @@
+    
+    //parameters
+function makeSandwhich(meat, cheese, bread) {
+    //work goes here
+    var newSandwhich = "here is your beautiful sandwhich, it has " + meat + " With " + cheese + " and " + bread;
+    // tell js to return
+    return newSandwhich; 
+}
+
+//call for run function with these specific input values "Arguments"
+var brendonsSandwhich = makeSandwhich("turkey", "pepperjack", "wheat");
+console.log(brendonsSandwhich);
